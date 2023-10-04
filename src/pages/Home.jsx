6d7f7849413713730c142a2294b9715ae8612086
Lucas <lucas.26.04@hotmail.com>
2023-10-04 19:06:19 -0300

    navBar PokemonCar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { Container, Grid } from "@mui/material";
+import NavBar from "../components/NavBar";
+import PokemonCard from "../components/PokemonCard";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <NavBar />
+      <Container maxWidth={false}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={2}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
